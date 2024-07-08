86f422105777cf380d455767e1c5d32b3b810376
fatih daşdelen <fatihdasdelen@hotmail.com>
2024-07-08 17:17:23 +0300

    Add project files.

---
+++ Services/Catalog/MultiShop.Catalog/Entities/Category.cs
@@ -0,0 +1,14 @@
+﻿namespace MultiShop.Catalog.Entities
+{
+    using MongoDB.Bson;
+    using MongoDB.Bson.Serialization.Attributes;
+
+    public class Category
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string CategoryID { get; set; }
+        public string CategoryName { get; set; }
+        
+    }
+}
